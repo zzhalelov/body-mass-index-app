8f3230dcc034c08d7446b72f05e04f9e60762cd7
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-31 23:50:31 +0600

    Colorize description's text depend on a BMI results

--- app/src/main/java/com/example/bodymassindex/MainActivity.java
+++ app/src/main/java/com/example/bodymassindex/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bodymassindex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -57,16 +58,22 @@ public class MainActivity extends AppCompatActivity {
 
     private String getBMIDescription(float bmi) {
         if (bmi < 18.5) {
+            textViewDescription.setTextColor(0xFF0000FF);
             return "Недостаточный вес";
         } else if (bmi >= 18.5 && bmi < 24.9) {
+            textViewDescription.setTextColor(0xFF00FF00);
             return "Здоровый вес";
         } else if (bmi >= 25 && bmi < 29.9) {
+            textViewDescription.setTextColor(0xFFFFA500);
             return "Предварительное ожирение";
         } else if (bmi >= 30 && bmi < 34.9) {
+            textViewDescription.setTextColor(0xFFDC143C);
             return "Ожирение I степени";
         } else if (bmi >= 35 && bmi < 39.9) {
+            textViewDescription.setTextColor(0xFFFF0000);
             return "Ожирение II степени";
         } else {
+            textViewDescription.setTextColor(0xFF800000);
             return "Ожирение III степени";
         }
     }
