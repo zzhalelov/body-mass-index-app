7db9afcab067b09bb8211041a03133394465e741
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-11-01 22:27:17 +0600

    Add app's icon; align the text

--- app/src/main/java/com/example/bodymassindex/MainActivity.java
+++ app/src/main/java/com/example/bodymassindex/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.bodymassindex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonCalculate;
     private TextView textViewResult;
     private TextView textViewDescription;
+    private TextView textViewMoreInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         buttonCalculate = findViewById(R.id.buttonCalculate);
         textViewResult = findViewById(R.id.textViewResult);
         textViewDescription = findViewById(R.id.textViewDescription);
+        textViewMoreInfo = findViewById(R.id.textViewMoreInfo);
+
         buttonCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,26 +59,34 @@ public class MainActivity extends AppCompatActivity {
             textViewResult.setText("Ошибка: Введите рост и вес.");
             textViewDescription.setVisibility(View.GONE);
         }
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(buttonCalculate.getWindowToken(), 0);
     }
 
     private String getBMIDescription(float bmi) {
         if (bmi < 18.5) {
             textViewDescription.setTextColor(0xFF0000FF);
+            textViewMoreInfo.setText("Ваш ИМТ менее 18,5 указывает на то, что у вас недостаточный вес, поэтому вам, возможно, придется немного прибавить в весе");
             return "Недостаточный вес";
         } else if (bmi >= 18.5 && bmi < 24.9) {
             textViewDescription.setTextColor(0xFF00FF00);
+            textViewMoreInfo.setText("Поздравляем! Ваш ИМТ указывает на то, что у вас нормальный вес для вашего роста. Поддерживая здоровый вес, вы снижаете риск развития серьезных проблем со здоровьем");
             return "Здоровый вес";
         } else if (bmi >= 25 && bmi < 29.9) {
             textViewDescription.setTextColor(0xFFFFA500);
+            textViewMoreInfo.setText("Ваш ИМТ от 25 до 29,9 указывает на то, что у вас небольшой избыточный вес. Вам могут порекомендовать похудеть по состоянию здоровья");
             return "Предварительное ожирение";
         } else if (bmi >= 30 && bmi < 34.9) {
             textViewDescription.setTextColor(0xFFDC143C);
+            textViewMoreInfo.setText("Ваш ИМТ более 30 указывает на то, что у вас большой избыточный вес. Ваше здоровье может оказаться под угрозой, если вы не похудеете");
             return "Ожирение I степени";
         } else if (bmi >= 35 && bmi < 39.9) {
             textViewDescription.setTextColor(0xFFFF0000);
+            textViewMoreInfo.setText("Ваш ИМТ более 30 указывает на то, что у вас большой избыточный вес. Ваше здоровье может оказаться под угрозой, если вы не похудеете");
             return "Ожирение II степени";
         } else {
             textViewDescription.setTextColor(0xFF800000);
+            textViewMoreInfo.setText("Ваш ИМТ более 30 указывает на то, что у вас большой избыточный вес. Ваше здоровье может оказаться под угрозой, если вы не похудеете");
             return "Ожирение III степени";
         }
     }
